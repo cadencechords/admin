979d1c5d14907eedba780272a0504b94bd96e253
Andrew Sidorchuk <sidorchukandrew@gmail.com>
2023-03-30 09:08:14 -0400

    show songs count

--- src/components/TeamSongsTab.js
+++ src/components/TeamSongsTab.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import useTeamSongs from '../hooks/api/useTeamSongs';
-import { Alert } from '@mantine/core';
+import { Alert, Text } from '@mantine/core';
 import LoadingIndicator from './LoadingIndicator';
 import SongRow from './SongRow';
+import { pluralize } from '../utils/string';
 
 export default function TeamSongsTab({ team }) {
   const { data: songs, isError } = useTeamSongs(team.id);
   if (songs) {
-    return songs.map(song => <SongRow song={song} key={song.id} />);
+    return (
+      <div>
+        <Text size="sm" c="dimmed" mb="xs">
+          {pluralize(songs, 'song')}
+        </Text>
+        {songs.length === 0 ? (
+          <Text>No songs to show</Text>
+        ) : (
+          songs.map(song => <SongRow song={song} key={song.id} />)
+        )}
+      </div>
+    );
   }
 
   if (isError) {
